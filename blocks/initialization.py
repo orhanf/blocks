e9763c7c419d8cc829c0e85f494e15a3504e0e09
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-06-03 17:03:38 -0400

    glarot initialization and removenotfinite changes

--- blocks/initialization.py
+++ blocks/initialization.py
@@ -223,3 +223,39 @@ class Sparse(NdarrayInitialization):
                                                  replace=False)
             weights[i, random_indices] = values[i]
         return weights
+
+
+class GlorotBengio(NdarrayInitialization):
+    """Initialize parameters with Glorot-Bengio method.
+
+    Use the following gaussian parameters: mean=0 and std=sqrt(scale/Nin).
+    In some circles this method is also called Xavier weight initialization.
+
+
+    Parameters
+    ----------
+    scale : float
+        1 for linear/tanh/sigmoid. 2 for RELU
+    normal : bool
+        Perform sampling from normal distribution. By defaut use uniform.
+
+    Notes
+    -----
+    For more information, see [GLOROT]_.
+
+    .. [GLOROT] Glorot et al. *Understanding the difficulty of training
+      deep feedforward neural networks*, International conference on
+      artificial intelligence and statistics, 249-256
+      http://jmlr.org/proceedings/papers/v9/glorot10a/glorot10a.pdf
+    """
+    def __init__(self, scale=1, normal=False):
+        self._scale = float(scale)
+        self._normal = normal
+
+    def generate(self, rng, shape):
+        w = numpy.sqrt(self._scale/shape[-1])
+        if self._normal:
+            m = rng.normal(0., w, size=shape)
+        else:
+            m = rng.uniform(-w, w, size=shape)
+        return m.astype(theano.config.floatX)
